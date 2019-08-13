7d6e053e04e1e11b4f18a808fbc96c31a3be7809
drdws <46658079+drdws@users.noreply.github.com>
2019-08-13 09:35:24 -0400

    Models  Bug Fix  Add Delete and Collection Methods

--- InMemory/MyShop.DataAccess.InMemory/ProductRepository.cs
+++ InMemory/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -12,7 +12,7 @@ namespace MyShop.DataAccess.InMemory
     public class ProductRepository
     {
         ObjectCache cache = MemoryCache.Default;
-        List<Product> products = new List<Product>();
+        List<Product> products ;
 
 
 
@@ -21,11 +21,10 @@ namespace MyShop.DataAccess.InMemory
             products = cache["products"] as List<Product>;
             if (products == null)
             {
-                products = new List<Product>()
+                products = new List<Product>();
             }
 
         }
-
 
         public void Commit()
         {
@@ -36,8 +35,6 @@ namespace MyShop.DataAccess.InMemory
         public void Insert(Product p)
         {
             products.Add(p);
-
-
         }
 
         public void Update(Product product)
@@ -53,8 +50,47 @@ namespace MyShop.DataAccess.InMemory
                 throw new Exception("Product not found");
             }
 
+        }
+
+        public Product Find(string Id)
+        {
+            Product product = products.Find(p => p.Id == Id);
+
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
 
         }
+
+
+        public void Delete(Product product)
+        {
+            Product productToDelete = products.Find(p => p.Id == product.Id);
+
+            if (productToDelete != null)
+            {
+                products.Remove(productToDelete);
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+
+        }
+
+
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+
+        }
+
+
     }
 
 }
---
+++ WebUI/WebUI/Controllers/ProductManagerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.Core.Models;
+using MyShop.DataAccess.InMemory;
+
+namespace WebUI.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        ProductRepository context;
+
+        public ProductManagerController()
+        {
+                       
+
+        }
+
+
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().to
+            return View();
+        }
+    }
+}
