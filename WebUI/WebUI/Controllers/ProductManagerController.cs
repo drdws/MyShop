d73cbe0bc210c3bcd12be5ea12d3d274298e5f5d
drdws <46658079+drdws@users.noreply.github.com>
2019-08-13 11:49:20 -0400

    ProductManager Built

--- WebUI/WebUI/Controllers/ProductManagerController.cs
+++ WebUI/WebUI/Controllers/ProductManagerController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using MyShop.Core.Models;
 using MyShop.DataAccess.InMemory;
+using WebUI.Controllers;
 
 namespace WebUI.Controllers
 {
@@ -14,16 +15,122 @@ namespace WebUI.Controllers
 
         public ProductManagerController()
         {
-                       
-
+            context = new ProductRepository();         
         }
 
 
         // GET: ProductManager
         public ActionResult Index()
         {
-            List<Product> products = context.Collection().to
-            return View();
+            List<Product> products = context.Collection().ToList<Product>();
+            return View(products);
         }
+
+
+        public ActionResult Create()
+        {
+            Product product = new Product();
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            else
+            {
+                context.Insert(product);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+
+
+        }
+
+        public ActionResult Edit(string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product, string Id)
+        {
+            Product p = context.Find(Id);
+            if (p == null)  
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(product);
+                }
+            }
+
+
+            p.Category = product.Category;
+            p.Description = product.Description;
+            p.Image = product.Image;
+            p.Name = product.Name;
+            p.Price = product.Price;
+
+            context.Commit();
+
+            return RedirectToAction("Index");
+
+        }
+
+
+        public ActionResult Delete(string Id)
+        {
+            Product p = context.Find(Id);
+            if (p == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(p);
+            }
+
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            Product p = context.Find(Id);
+            if (p == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(p);
+                context.Commit();
+                return RedirectToAction("Index");
+                                
+            }
+
+
+        }
+
     }
+
+    
+
 }
